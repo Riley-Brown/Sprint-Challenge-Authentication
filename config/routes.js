5b010a07f9fbe74770cd02f2f33a72d9078f6612
Riley-Brown <riley@riley.gg>
2018-11-30 13:00:25 -0500

    Added jwt token to register and login

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,79 @@
-const axios = require('axios');
-
-const { authenticate } = require('./middlewares');
+const axios = require("axios");
+const keys = require("../_secrets/keys");
+const { authenticate } = require("./middlewares");
+const bcrypt = require("bcryptjs");
+const knex = require("knex");
+const knexConfig = require("../knexfile");
+const db = knex(knexConfig.development);
+const jwt = require("jsonwebtoken");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
+  server.get("/", test);
 };
+
+// generate jwt token
+// generate JWT token
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: ["marketing", "development"]
+  };
+
+  const secret = keys.jwtKey;
+  const options = {
+    expiresIn: "1h"
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const token = generateToken(ids);
+      res.status(201).json({ id: ids[0], token });
+    })
+    .catch(err => res.status(500).json(err));
+}
+function test(req, res) {
+  res.json({ message: "api up" });
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ id: user.id, token });
+      } else {
+        res.status(401().json({ message: "you shall not pass" }));
+      }
+    });
 }
 
 function getJokes(req, res) {
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get("https://safe-falls-22549.herokuapp.com/random_ten")
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
